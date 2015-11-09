3ab1266fae6413864a377d4a0e469581235899d6
Ian Eslick <eslick@alum.mit.edu>
2015-11-09 13:26:15 -0800

    Bump versions
    - 1.0.0 since we've been stable for awhile

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject com.vitalreactor/twilio-rest "0.9.2"
+(defproject com.vitalreactor/twilio-rest "1.0.0"
   :min-lein-version "2.0.0"
   :description "Adapter library for the Twilio web service."
   :url "http://github.com/vitalreactor/twilio-rest"
   :scm "http://github.com/vitalreactor/twilio-rest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cheshire "5.2.0"]
-                 [clj-http "0.7.7"]
-                 [midje    "1.5.0"]]
-  :plugins [[lein-midje "3.0.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [cheshire "5.3.1"]
+                 [clj-http "2.0.0"]
+                 [midje    "1.8.2"]]
+  :plugins [[lein-midje "3.1.3"]])
